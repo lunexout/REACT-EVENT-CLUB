97e310a90fb0fd918a864e2e9999307987321da2
Jano Gazashvili <69036038+lunexout@users.noreply.github.com>
2021-02-14 23:24:37 +0400

    init
    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>HELLO EVENT CLUB</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/connectFirebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp ({
+    apiKey: "AIzaSyCGHzo2p7YBnMx8X2HxnYFOmo8cM7v5kas",
+    authDomain: "event-club-d3249.firebaseapp.com",
+    projectId: "event-club-d3249",
+    storageBucket: "event-club-d3249.appspot.com",
+    messagingSenderId: "142973268261",
+    appId: "1:142973268261:web:012626c4ea5930ed41eacc",
+    measurementId: "G-ELVM8P2RSS"
+})
+
+const db = firebaseApp.firestore()
+
+export default db;
